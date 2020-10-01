71ef16a3b93c1aa5223301d75dedffc5bb9bdb49
H352615 <dhawan.rachakonda@honeywell.com>
2020-10-02 03:40:54 +0530

    feat: example outlook UI

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+
+import {getStore} from 'redux/storeConfig';
+
+(async function () {
+  const store = await getStore();
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <App />
+            </Route>
+          </Switch>
+          <App />
+        </Router>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+})();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
